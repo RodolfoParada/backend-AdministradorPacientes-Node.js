28c9f033b26a70c535dfda540a2f86f32bc4c8db
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-03-01 14:34:16 -0300

    cambio en db para deploy

--- config/db.js
+++ config/db.js
@@ -1,37 +1,37 @@
-//  import mongoose from 'mongoose';
+ import mongoose from 'mongoose';
 
-// const conectarDB = async () => {
-//      try {
-//         const db  = await mongoose.connect(process.env.MONGO_URI,{
-//               useNewUrlParser: true,
-//              useUnifiedTopology: true,
+ const conectarDB = async () => {
+      try {
+         const db  = await mongoose.connect(process.env.MONGO_URI,{
+               useNewUrlParser: true,
+              useUnifiedTopology: true,
            
-//          });
+          });
+          const url = `${db.connection.host}:${db.connection.port}`;
+          console.log(`Conectado a la base de datos en: ${url}`);
+      } catch (error) {
+          console.log(`error: ${error.message}`);
+          process.exit(1); // Detener la app
+      }
+  }
+
+export default conectarDB;
+
+// import mongoose from 'mongoose';
+
+//  const conectarDB = async () => {
+//      try {
+//          // Conexión a MongoDB
+//          const db = await mongoose.connect(process.env.MONGO_URI);
+        
+
+//          // Formateo de la URL para confirmación
 //          const url = `${db.connection.host}:${db.connection.port}`;
 //          console.log(`Conectado a la base de datos en: ${url}`);
 //      } catch (error) {
-//          console.log(`error: ${error.message}`);
-//          process.exit(1); // Detener la app
+//          console.error(`Error al conectar a la base de datos: ${error.message}`);
+//          process.exit(1); // Detener la app en caso de error crítico
 //      }
-//  }
+//  };
 
-//export default conectarDB;
-
-import mongoose from 'mongoose';
-
- const conectarDB = async () => {
-     try {
-         // Conexión a MongoDB
-         const db = await mongoose.connect(process.env.MONGO_URI);
-        
-
-         // Formateo de la URL para confirmación
-         const url = `${db.connection.host}:${db.connection.port}`;
-         console.log(`Conectado a la base de datos en: ${url}`);
-     } catch (error) {
-         console.error(`Error al conectar a la base de datos: ${error.message}`);
-         process.exit(1); // Detener la app en caso de error crítico
-     }
- };
-
- export default conectarDB;
+//  export default conectarDB;
