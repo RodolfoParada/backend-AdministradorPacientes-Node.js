d17f537bace153c3ecf4391401886b91475e8b3e
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-03-01 14:48:44 -0300

    mejora2 en bd.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
- import mongoose from 'mongoose';
+//  import mongoose from 'mongoose';
 
 //  const conectarDB = async () => {
 //       try {
@@ -34,4 +34,4 @@ import mongoose from 'mongoose';
       }
   };
 
-//  export default conectarDB;
+  export default conectarDB;
